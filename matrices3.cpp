e1faa5c8a2a8321635fb30f80b3abc17b1c6d6a2
ldcruzl <lcruzl@unjbg.edu.pe>
2024-07-15 18:43:44 -0500

    Actualizacion matrices3: Se completo con la lectura del orden de la matriz y con la salida, se usaron estructuras for, dentro de otro for

--- matrices3.cpp
+++ matrices3.cpp
@@ -4,5 +4,25 @@
 using namespace std;
 
 int main(){
+    int n;
+    cout<<"Ingrese el tamanio de la matriz: ";cin>>n;
+    int matriz[n][3*n];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<3*n; j++){
+            if(i==0||i==n-1||j==0||j==(3*n)-1){
+                matriz[i][j]=1;
+            }
+            else{
+                matriz[i][j]=0;
+            }
+        }
+    }
+    cout<<"-----Matriz resultante-----"<<endl;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<3*n; j++){
+            cout<<matriz[i][j];
+        }
+        cout<<endl;
+    }
     return 0;
 }
