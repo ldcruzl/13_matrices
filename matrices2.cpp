e1088bcb6a99743fd567de45eb85c8e2cad5972c
ldcruzl <lcruzl@unjbg.edu.pe>
2024-07-15 10:46:51 -0500

    Actualizacion matrices2: Se completo la correcta de datos, usando un doble for

--- matrices2.cpp
+++ matrices2.cpp
@@ -4,5 +4,13 @@
 using namespace std;
 
 int main(){
+    int n, s=0;
+    int matriz[n][n];
+    cout<<"Ingres el orden de la matriz: ";cin>>n;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout<<"Ingrese el valor para ["<<i+1<<"]"<<" ["<<j+1<<"]: ";cin>>matriz[i][j];
+        }
+    }
     return 0;
 }
