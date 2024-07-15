a5a384936dd92f953e6d10f13e19a4200fd9d257
ldcruzl <lcruzl@unjbg.edu.pe>
2024-07-15 11:29:37 -0500

    Actualizacion matrices2: Se culmino con la sumatoria de la diagonal secundaria, usando un doble for, despues un if para la condicion

--- matrices2.cpp
+++ matrices2.cpp
@@ -5,12 +5,21 @@ using namespace std;
 
 int main(){
     int n, s=0;
-    int matriz[n][n];
     cout<<"Ingres el orden de la matriz: ";cin>>n;
+    int matriz[n][n];
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
             cout<<"Ingrese el valor para ["<<i+1<<"]"<<" ["<<j+1<<"]: ";cin>>matriz[i][j];
         }
     }
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(i+j==n-1){
+                s=s+matriz[i][j];
+            }
+        }
+    }
+    cout<<"-------Sumatoria de diagonal secundaria-------"<<endl,
+    cout<<"La sumatoria es de: "<<s<<endl;
     return 0;
 }
