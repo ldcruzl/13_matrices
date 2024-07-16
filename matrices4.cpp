6245b20da649a88c6c44d1c629209378322a3247
ldcruzl <lcruzl@unjbg.edu.pe>
2024-07-15 20:00:45 -0500

    Actualizacion matrices4: Se completo con la correcta digitalizacion de datos para cada dia y semana correspondiente

--- matrices4.cpp
+++ matrices4.cpp
@@ -4,5 +4,12 @@
 using namespace std;
 
 int main(){
+    int n=7, m=4;
+    int ventas[n][m];
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            cout<<"Ingrese la venta de dia"<<j+1<<" sem"<<i+1<<": ";cin>>ventas[i][j];
+        }
+    }
     return 0;
 }
