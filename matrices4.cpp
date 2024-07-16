cb1cdd61ff8c2080c6e04966155859465f3221be
ldcruzl <lcruzl@unjbg.edu.pe>
2024-07-15 20:05:46 -0500

    Actulizacion matrices4: Se completo la implementacion de la sumatoria de los valores ingresados

--- matrices4.cpp
+++ matrices4.cpp
@@ -11,5 +11,12 @@ int main(){
             cout<<"Ingrese la venta de dia"<<j+1<<" sem"<<i+1<<": ";cin>>ventas[i][j];
         }
     }
+    int s=0;
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            s=s+ventas[i][j];
+        }
+    }
+    cout<<"Venta total del mes: "<<s<<endl;
     return 0;
 }
