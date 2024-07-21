3c3c9c9dd68c27e4aa3a4a7d1d308e3e7c8bd855
ldcruzl <lcruzl@unjbg.edu.pe>
2024-07-20 22:39:14 -0500

    Actualizacion matrices5: Se completo con la lectura de las ciudades y los partidos politicos

--- matrices5.cpp
+++ matrices5.cpp
@@ -4,5 +4,19 @@
 using namespace std;
 
 int main(){
+    int n, m;
+    cout<<"Ingrese el numero de partidos politicos: ";cin>>n;
+    cout<<"Ingrese el numero de ciudades: ";cin>>m;
+    string par[n];
+    string ciu[m];
+    int votos[n][m];
+    for(int i=0; i<n; i++){
+        cin.ignore();
+        cout<<"Ingrese el nombre del partido #"<<i+1<<" :";getline(cin,par[i]);
+    }
+    for(int i=0; i<m; i++){
+        cin.ignore();
+        cout<<"Ingrese el nombre de la ciudad #"<<i+1<<": ";getline(cin,ciu[i]);
+    }
     return 0;
 }
