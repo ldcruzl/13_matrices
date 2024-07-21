c6702a55a7cf15a86af761bf8475e844c0601964
ldcruzl <lcruzl@unjbg.edu.pe>
2024-07-20 22:03:13 -0500

    Actualizacion matrices4: Se completo con la salida de la matriz junto a la sumatoria total

--- matrices4.cpp
+++ matrices4.cpp
@@ -4,19 +4,28 @@
 using namespace std;
 
 int main(){
-    int n=7, m=4;
+    int n = 7, m = 4;
     int ventas[n][m];
-    for(int i=0; i<m; i++){
-        for(int j=0; j<n; j++){
-            cout<<"Ingrese la venta de dia"<<j+1<<" sem"<<i+1<<": ";cin>>ventas[i][j];
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cout << "Ingrese la venta del dia: ";
+            cin >> ventas[i][j];
         }
     }
-    int s=0;
-    for(int i=0; i<m; i++){
-        for(int j=0; j<n; j++){
-            s=s+ventas[i][j];
+    int s = 0;
+    cout<<"Venta de cuatro semanas"<<endl;
+    cout<<"---------------------------------"<<endl;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            s += ventas[i][j];
         }
     }
-    cout<<"Venta total del mes: "<<s<<endl;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cout << ventas[i][j] << "  ";
+        }
+        cout << endl;
+    }
+    cout << "Venta total del mes: " << s << endl;
     return 0;
 }
